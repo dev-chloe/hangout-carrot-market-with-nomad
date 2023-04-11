d4ebdd795004139170ea1c55b4b2902edf0a59c5
dev-chloe <chloekim.4969@gmail.com>
2023-04-11 22:27:08 +0900

    Clean Code part Two

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -4,9 +4,31 @@ export default function useMutaion(url: string): [
   (data: any) => void, 
   {loading: boolean; data: undefined | any; error: undefined | any}
 ] {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data: any) {}
-  return [mutation, { loading, data, error }]
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  })
+  // const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState<undefined | any>(undefined);
+  // const [error, setError] = useState<undefined | any>(undefined);
+  function mutation(data: any) {
+    setState((prev) => ({...prev, loading: true}))
+    // setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json().catch(() => {}))
+    .then((data) => setState((prev) => ({...prev, data})))
+    .catch((error) => setState((prev) => ({...prev, error})))
+    .finally(() => setState((prev) => ({...prev, loading: false})));
+    // .then(setData)
+    // .catch(setError).finally(() => setLoading(false))
+  }
+  return [mutation, { ...state }]
+  // return [mutation, { loading, data, error }]
 }
