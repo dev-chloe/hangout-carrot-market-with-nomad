0db0f321aa823b6720b1ac6611284139f8b8aba9
dev-chloe <chloekim.4969@gmail.com>
2023-04-16 14:54:33 +0900

    Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -8,57 +8,22 @@ async function handler (
 ){
   const {phone, email} = req.body;
   const payload = phone ? {phone: +phone} : {email};
-  const user = await client.user.upsert({
-    where: {
-      ...payload
-      // ...(phone ? {phone: +phone} : {}),
-      // ...(email ? {email} : {}),
-    },
-    create: {
-      name: "Anonymous",
-      ...payload
-      // ...(phone ? {phone: +phone} : {}),
-      // ...(email ? {email} : {}),
-    },
-    update: {}
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload
+          },
+          create: {
+            name: "Anonymous",
+            ...payload
+          },
+        }
+      }
+    }
   })
-  console.log(user);
-  // if(email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email
-  //     },
-  //   });
-  //   if(user) console.log("found it")
-  //   if(!user) {
-  //     console.log("not find")
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         email,
-  //       }
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  // if(phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone
-  //     },
-  //   });
-  //   if(user) console.log("found it")
-  //   if(!user) {
-  //     console.log("not find")
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "Anonymous",
-  //         phone: +phone,
-  //       }
-  //     });
-  //   }
-  //   console.log(user);
-  // }
   return res.status(200).end();
 }
 
