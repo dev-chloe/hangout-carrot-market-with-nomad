1e497d176bf39a1c2c158b4de9ab53a6016b6d8d
dev-chloe <chloekim.4969@gmail.com>
2023-04-17 22:03:44 +0900

    Profile Handler

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,22 +15,19 @@ async function handler (
   req:NextApiRequest, 
   res:NextApiResponse<ResponseType>,
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
+  console.log(req.session.user)
+  const profile = await client.user.findUnique({
     where: {
-      payload: token,
-    },
-    // include: {user: true} > 연관된 테이블에서의 정보도 볼 수 있게 해 줌
+      id: req.session.user?.id
+    }
   })
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId
-  }
-  await req.session.save();
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password: `${process.env.IRON_PASSWORD}`
 }) ;
