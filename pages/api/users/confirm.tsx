dc441f7aa23aed8c0b53a1148f984d9d83e22a4f
dev-chloe <chloekim.4969@gmail.com>
2023-04-17 23:00:36 +0900

    Cleaning Code

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,36 +1,31 @@
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withApiSession } from "@/libs/server/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    }
-  }
-}
 
 async function handler (
   req:NextApiRequest, 
   res:NextApiResponse<ResponseType>,
 ) {
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
-    // include: {user: true} > 연관된 테이블에서의 정보도 볼 수 있게 해 줌
   })
-  if (!exists) return res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists.userId
+    id: foundToken.userId
   }
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId
+    }
+  })
+  res.json({
+    ok: true
+  });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password: `${process.env.IRON_PASSWORD}`
-}) ;
+export default withApiSession(withHandler("POST", handler)) ;
