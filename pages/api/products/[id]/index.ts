3934da92e1f1e2333ac6f713e1ab29031327af01
dev-chloe <chloekim.4969@gmail.com>
2023-04-23 19:18:17 +0900

    Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,7 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
 ) {
-  const { id } = req.query;
+  const { query: { id }, session: { user } } = req;
   const product = await client.product.findUnique({
     where: {
       id: Number(id),
@@ -37,9 +37,21 @@ async function handler(
       },
     },
   });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id
+      },
+      select: {
+        id: true
+      }
+    })
+  )
   res.json({
     ok: true,
     product,
+    isLiked,
     relatedProducts
   })
 }
